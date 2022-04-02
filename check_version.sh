2a6335dc2b940efda19e8121fa89d1de920b720e
alevan <levan.andrew.w@gmail.com>
2022-04-02 12:49:33 -0400

    cleaning up the repo

--- check_version.sh
+++ check_version.sh
@@ -3,6 +3,7 @@ tag_list=$(curl levan.home:5000/v2/horizons-api/tags/list)
 if [[ ${tag_list} == *"$(poetry version -s)"* ]]; then
     echo "Bumping Horizons API version"
     poetry version patch
+    exit 1
 else
     echo "Horizons API version ok"
 fi
