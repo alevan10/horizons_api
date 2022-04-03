f1f9426111ad77a8bff30bbfa61de84c58f1ef24
alevan <levan.andrew.w@gmail.com>
2022-04-03 11:35:04 -0400

    working on api models

--- src/horizons_web_service/models.py
+++ src/horizons_web_service/models.py
@@ -1,9 +1,14 @@
 from typing import Union
 
-from horizons_client.entities.enums import Moons, Planets
-from pydantic.main import BaseModel
+from horizons_client.entities.enums import Moons, Planets, ResponseOptions
+from pydantic import BaseModel
+
+
+class EphemerideOptionsRequest(BaseModel):
+    pass
 
 
 class EphemerideRequest(BaseModel):
 
     target = Union[Planets, Moons]
+    options = list(ResponseOptions)
