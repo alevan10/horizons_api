8b2c9d80f4330be817c04ec1cc1072013b4213ea
alevan <levan.andrew.w@gmail.com>
2022-04-03 12:54:49 -0400

    Finding some issues in the client I need to improve on

--- src/horizons_web_service/blueprint.py
+++ src/horizons_web_service/blueprint.py
@@ -1,4 +1,10 @@
+from typing import Union
+
 from fastapi import APIRouter
+from horizons_client.entities.enums import Moons, Planets
+from horizons_client.services.horizons_request_service import HorizonsRequestService
+
+from horizons_web_service.models import EphemerideRequest, EphemerideResponse
 
 horizons_router = APIRouter()
 
@@ -9,5 +15,12 @@ async def health_endpoint() -> str:
 
 
 @horizons_router.post("/ephemerides")
-async def ephemerides_endpoint() -> str:
+async def ephemerides_endpoint(
+    requests: list[EphemerideRequest],
+) -> dict[Union[Planets, Moons] : list[EphemerideResponse]]:
+    responses = {}
+    for request in requests:
+        horizons_svc = HorizonsRequestService(request.to_horizons_request())
+        horizons_res = await horizons_svc.make_request()
+        responses.update({request.target: [EphemerideResponse(**dict(res)) for res in horizons_res]})
     return "pong"
--- src/horizons_web_service/models.py
+++ src/horizons_web_service/models.py
@@ -1,14 +1,55 @@
-from typing import Union
+from datetime import datetime, timedelta
+from typing import Optional, Union
 
-from horizons_client.entities.enums import Moons, Planets, ResponseOptions
-from pydantic import BaseModel
+from horizons_client.entities.enums import (
+    AngleFormat,
+    EphemerideOptions,
+    Moons,
+    Observers,
+    Planets,
+    ResponseOptions,
+    StepSize,
+)
+from horizons_client.services.request_objects import (
+    BaseRequestObject,
+    CenterRequestObject,
+    CommandRequestObject,
+    StartTimeRequest,
+    StopTimeRequest,
+)
+from pydantic import BaseModel, Field
 
 
 class EphemerideOptionsRequest(BaseModel):
-    pass
+    angle_format: AngleFormat = Field(default=AngleFormat.DEG)
+    step_site: StepSize = Field(default=StepSize.HOUR)
+    options: list[EphemerideOptions] = Field(
+        default=[EphemerideOptions.APPARENT_RA_AND_DEC]
+    )
 
 
 class EphemerideRequest(BaseModel):
+    target: Union[Planets, Moons]
+    observer: Observers = Field(default=Observers.SUN)
+    return_options: EphemerideOptionsRequest
+    start_time: datetime = Field(default=datetime.utcnow())
+    end_time: datetime = Field(default=datetime.utcnow() + timedelta(days=1))
 
-    target = Union[Planets, Moons]
-    options = list(ResponseOptions)
+    def to_horizons_request(self) -> list[BaseRequestObject]:
+        command = CommandRequestObject(value=self.target)
+        center = CenterRequestObject(value=self.observer)
+        start = StartTimeRequest(value=self.start_time)
+        stop = StopTimeRequest(value=self.end_time)
+        return [command, center, start, stop]
+
+
+class EphemerideResponse(BaseModel):
+    date: datetime
+    ra_icrf: float
+    dec_icrf: float
+    dev_a_app: float
+    d_ra: float
+    ap_mag: float
+    s_brt: float
+    delta: float
+    deldot: float
