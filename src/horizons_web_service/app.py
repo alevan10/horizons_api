79d3dc40ef17cf1f50823cdc070a8a80e1da2d15
alevan <levan.andrew.w@gmail.com>
2022-06-05 12:53:41 -0400

    updating cors

--- src/horizons_web_service/app.py
+++ src/horizons_web_service/app.py
@@ -1,10 +1,22 @@
+import os
+
 from blueprint import horizons_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+default_origins = [
+    "http://levan.home",
+    "https://levan.home",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 
 def create_app() -> FastAPI:
     horizons_app = FastAPI()
     horizons_app.include_router(horizons_router)
+    default_origins.extend(os.environ.get("ALLOWED_ORIGINS", []))
+    horizons_app.add_middleware(CORSMiddleware, allow_origins=default_origins)
     return horizons_app
 
 
