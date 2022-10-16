d881b26d2438032872e5d6b9f7f5bd7d2915caa6
alevan <levan.andrew.w@gmail.com>
2022-10-16 15:30:07 -0400

    updating pipeline

---
+++ wait_for_endpoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+url=${HORIZONS_API_URL:="http://levan.home/api/v0"}
+count=0
+response=$(curl --silent $url/health)
+while [ "$response" != "\"pong\"" ]
+do
+  if [ $count -gt 20 ]; then
+    echo "Endpoint unavailable"
+    exit 2
+  fi
+  response=$(curl --silent $url/health)
+  ((count+=1))
+  echo "Attempt $count. Retrying..."
+  sleep 5
+done
+echo "Endpoint is ready"
